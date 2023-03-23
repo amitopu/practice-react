3160516195ad5299575d73b7f64c93d2ff49d1d5
rupok <nurealamsiddiky@gmail.com>
2023-03-23 10:55:02 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
 
+// header component
+function Header() {
+    return <h1>Hello world</h1>;
+}
+
+// logo component
+function Logo({ darkMode }) {
+    const imageStyle = {
+        width: "100px",
+        height: "100px",
+        backgroundColor: `${darkMode ? "black" : "white"}`,
+    };
+    const image = <img src={logo} style={imageStyle} alt="" />;
+
+    return image;
+}
+
+const ModeChangeButton = ({ mode, change }) => {
+    let darkModeOn = <p>Dark mode is on</p>;
+    let darkModeOff = <p>Dark mode is off</p>;
+    const modeChangeHandler = () => {
+        change((mode) => !mode);
+        if (!mode) {
+            console.log("Dark mode is turned on");
+        } else {
+            console.log("Dark mode is turned off");
+        }
+    };
+
+    return (
+        <>
+            {mode ? darkModeOn : darkModeOff}
+            <button onClick={modeChangeHandler}>change mode</button>
+        </>
+    );
+};
+
+// app component or main component
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [darkMode, setDarkMode] = useState(false);
+    return (
+        <>
+            <Header />
+            <Logo darkMode={darkMode}></Logo>
+            <br />
+            <ModeChangeButton
+                mode={darkMode}
+                change={setDarkMode}
+            ></ModeChangeButton>
+        </>
+    );
 }
 
 export default App;
